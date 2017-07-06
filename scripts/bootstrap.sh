589e599551bd71e26059c818ecb28f6a0e285982
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-07-06 20:19:51 +0100

    fixed supervisor install

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -12,3 +12,7 @@ apt-get -y install apache2
 #use apache configs on hosts machine
 rm /etc/apache2/apache2.conf
 ln -s /resources/apache/apache2.conf /etc/apache2/apache2.conf
+
+#install supervisord
+apt-get install python-setuptools
+easy_install supervisor
